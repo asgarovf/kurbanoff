b61f5c02cef50bd2c1efb67ddd83613f6803a5bb
ituitis-asgarovf19 <fasgarov2002@gmail.com>
2020-05-10 18:57:45 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,84 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    navText : ["<i class='fa fa-chevron-left'>&nbsp;&nbsp;</i>","<i class='fa fa-chevron-right'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    },
+    autoplay:true,
+    autoplayTimeout:4000,
+})
+
+$(document).ready(function(){
+    $("body").fadeIn(800)
+
+    $("#box_1").mouseover(function(){
+        $(this).css("background","#463810");
+        $(".box_title_1").css("color","white");
+        $(".box_text_1").css("color","white");
+    }),
+    $("#box_1").mouseout(function(){
+        $(this).css("background","#efefef");
+        $(".box_title_1").css("color","#5c3b10");
+        $(".box_text_1").css("color","black")
+    });
+
+    $("#box_2").mouseover(function(){
+        $(this).css("background","#463810");
+        $(".box_title_2").css("color","white");
+        $(".box_text_2").css("color","white")
+    }),
+    $("#box_2").mouseout(function(){
+        $(this).css("background","#efefef");
+        $(".box_title_2").css("color","#5c3b10");
+        $(".box_text_2").css("color","black")
+    });
+
+    $("#box_3").mouseover(function(){
+        $(this).css("background","#463810");
+        $(".box_title_3").css("color","white");
+        $(".box_text_3").css("color","white")
+    }),
+    $("#box_3").mouseout(function(){
+        $(this).css("background","#efefef");
+        $(".box_title_3").css("color","#5c3b10");
+        $(".box_text_3").css("color","black")
+    });
+
+    $("#box_4").mouseover(function(){
+        $(this).css("background","#463810");
+        $(".box_title_4").css("color","white");
+        $(".box_text_4").css("color","white")
+    }),
+    $("#box_4").mouseout(function(){
+        $(this).css("background","#efefef");
+        $(".box_title_4").css("color","#5c3b10");
+        $(".box_text_4").css("color","black")
+    });
+
+    $("#box_1").hover(function(){
+        $(this).animate({bottom:"10px",});
+        $(this).animate({top:"0px",});
+    });
+    $("#box_2").hover(function(){
+        $(this).animate({bottom:"10px",});
+        $(this).animate({top:"0px",}); 
+    });
+    $("#box_3").hover(function(){
+        $(this).animate({bottom:"10px",});
+        $(this).animate({top:"0px",});
+    });
+    $("#box_4").hover(function(){
+        $(this).animate({bottom:"10px",});
+        $(this).animate({top:"0px",});
+    });
+});
